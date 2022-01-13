6c411e3162e7e616513056a6c85b494dde1a28d1
kasian777 <kasian.dima@ukr.net>
2022-01-13 11:46:12 +0200

    2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Users from "./components/user/users";
 import Posts from "./components/post/posts";
 import Comments from "./components/coment/comments";
 
-
 function App() {
     return (
         <div>
