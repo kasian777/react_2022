e22a96fafa9929f07396b4b36b723c5f5295862f
kasian777 <kasian.dima@ukr.net>
2022-01-20 00:59:05 +0200

    new4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form>
+            <div><label>Name:<input type='text' name={'name'}/></label></div>
+            <div><label>Age:<input type='text' name={'age'}/></label></div>
+            <button>Find</button>
+        </form>
     </div>
   );
 }
