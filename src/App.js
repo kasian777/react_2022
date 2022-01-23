e1e15d2ff205002617b1e46fd6746807c6b11f77
kasian777 <kasian.dima@ukr.net>
2022-01-23 15:05:10 +0200

    1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
         <form>
             <div><label>Name:<input type='text' name={'name'}/></label></div>
             <div><label>Age:<input type='text' name={'age'}/></label></div>
+
             <button>Find</button>
         </form>
     </div>
